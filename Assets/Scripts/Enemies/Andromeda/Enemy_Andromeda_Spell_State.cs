1a6e0fa72befaf630f12baeed588e7dc5549d13c
Bao <nhanhem2ui@gmail.com>
2026-01-28 17:22:25 +0700

    [ADD]: Mass Fx Andromeda

---
+++ Assets/Scripts/Enemies/Andromeda/Enemy_Andromeda_Spell_State.cs
@@ -0,0 +1,18 @@
+﻿public enum Enemy_Andromeda_Spell_State
+{
+    [StateAnimator("Empty")]
+    Empty,
+
+    [StateAnimator("CoreEffect")]
+    CoreAnimation,
+
+    [StateAnimator("Effect1")]
+    Windblade,
+
+    [StateAnimator("Effect2")]
+    Smoke,
+
+    [StateAnimator("Effect3")]
+    Swirl
+
+}
